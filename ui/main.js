6b5ba80bbcce9aa37da094a09139b190b4fe77af
Abhishek Jindal <32256963+jindalabhishek1@users.noreply.github.com>
2018-03-11 11:39:34 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -13,6 +13,6 @@ function moveRight() {
     marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 };
-image.onclick = function() {
+img.onclick = function() {
     var interval = setInterval(moveRight, 50);
 };
