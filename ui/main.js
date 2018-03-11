5d2b3c505cdc51d3401c2622cd9e9c4898dcf5c3
Abhishek Jindal <32256963+jindalabhishek1@users.noreply.github.com>
2018-03-11 21:46:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,4 @@
+/*
 console.log('Loaded!');
 
 // changing the content to main-txt div
@@ -16,4 +17,38 @@ function moveRight() {
 
 img.onclick = function() {
     var interval = setInterval(moveRight, 50);
+};
+*/
+// Counter code
+
+var button = document.getElementById('counter');
+//var counter = 0;
+
+button.onclick = function() {
+    //create a request object.
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable.
+    request.onreadystatechange = function() {
+        if(request.readyState == XMLHttpRequest.DONE)
+        {
+            //take some action
+            if(request.status == 200)
+            {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        //if not do nothing
+    };
+    
+    //make a request
+    request.open('GET', 'http://abhishek1036cse16.imad.hasura-app.io/counter', true);
+    request.send(null);
+    
+    //render the variable in the correct span.
+    //counter = counter + 1;
+    //var span = document.getElementById('count');
+    //span.innerHTML = counter.toString();
 };
