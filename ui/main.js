e0c5360797d988d9cb85a954f21bece9aa16f909
Abhishek Jindal <32256963+jindalabhishek1@users.noreply.github.com>
2018-03-11 11:16:49 +0530

    Update main.js
    added js code to move image and change content of main-txt div

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,18 @@
 console.log('Loaded!');
+
+// changing the content to main-txt div
+
+var element = document.getElementById('main-txt');
+element.innerHTML = "New Text";
+
+// Moving the image to right when clicked
+    
+var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
+};
+image.onclick = function() {
+    var interval = setInterval(moveRight, 50);
+};
