6c8bbef18bfbafa04cbd1cba3fd4a983c298a2fa
Abhishek Jindal <32256963+jindalabhishek1@users.noreply.github.com>
2018-03-21 18:08:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,6 +109,18 @@ app.post('/create-user', function (req, res) {
        if (err){
             res.status(500).send(err.toString());
         } else {
+            res.send('User successfully created: ' + username);
+        }
+   });
+});
+
+app.post('/login', function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    pool.query('SELECT * FROM "user" username = $1', [username], function (err, result) {
+   if (err){
+            res.status(500).send(err.toString());
+        } else {
             if (result.rows.length === 0)
             {
                 res.send(403).send('username/password incorrect....');
@@ -125,18 +137,6 @@ app.post('/create-user', function (req, res) {
                     res.send('username/password is invalid...');
                 }
             }
-        }
-   });
-});
-
-app.post('/login', function (req, res) {
-    var username = req.body.username;
-    var password = req.body.password;
-    pool.query('SELECT * FROM "user" username = $1', [username], function (err, result) {
-       if (err){
-            res.status(500).send(err.toString());
-        } else {
-            res.send('User successfully created: ' + username);
         }
    });
 });
