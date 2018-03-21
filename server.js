00ac75da6b946c589981121170c8b9287dee670c
Abhishek Jindal <32256963+jindalabhishek1@users.noreply.github.com>
2018-03-21 15:55:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/', function (req, res) {
 
 function hash (input, salt) {
     // how to create a hash
-    var hashed = crypto.pbkdf2(input, salt, 10000, 512, 'sha512');
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return hashed.toString('hex');
 }
 app.get('/hash/:input', function (req, res) {
