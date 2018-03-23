ff816aa8a6c93dff544b96bd68967d5b74c90c4d
Abhishek Jindal <32256963+jindalabhishek1@users.noreply.github.com>
2018-03-23 11:57:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ app.post('/login', function (req, res) {
     var password = req.body.password;
     pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
     if (err) {
-            res.status(500).send(err.toString());
+            res.send(500).send(err.toString());
         } else {
             if (result.rows.length === 0) {
                 res.status(403).send('username/password incorrect..');
@@ -131,7 +131,7 @@ app.post('/login', function (req, res) {
                 if (hashedPassword === dbString) {
                     res.send('credentials correct...!!!');
                 } else {
-                    res.status(403).send('username/password is invalid...');
+                    res.send(403).send('username/password is invalid...');
                 }
             }
         }
