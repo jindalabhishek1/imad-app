f418e7bdaf34670b29a5a170917b424cdd607c25
Abhishek Jindal <32256963+jindalabhishek1@users.noreply.github.com>
2018-05-10 01:42:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -62,7 +62,7 @@ function loadLoginForm() {
 function loadLoggedInUser (username) {
     var loginArea = document.getElementById('login_area');
     loginArea.innerHTML = `
-        <h3> Hi <i>${username}.toUpperCase()</i></h3>
+        <h3> Hi <i>${username}</i></h3>
         <a href="/logout">Logout</a>
     `;
 }
