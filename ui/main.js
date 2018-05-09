cdd0b8bf30acdd20ea3dde0832ad738d4e7ba241
Abhishek Jindal <32256963+jindalabhishek1@users.noreply.github.com>
2018-05-09 17:53:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -80,7 +80,8 @@ submit.onclick = function() {
                 }
                 var ul = document.getElementById('namelist');
                 ul.innerHTML = list;*/
-                res.redirect('/UserHomePage');
+                var res;
+                res.send('<script>window.location.href="your URL";</script>');
             } else if (request.status === 403) {
                 console.log(username, password);
                 alert('Username/password is incorrect');
