9e421aada380fdbe79ab72a1570ba7c63e0b0fc1
Abhishek Jindal <32256963+jindalabhishek1@users.noreply.github.com>
2018-05-09 21:35:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,109 +1,80 @@
-/*
-console.log('Loaded!');
-
-// changing the content to main-txt div
-
-var element = document.getElementById('main-txt');
-element.innerHTML = "New Text";
-
-// Moving the image to right when clicked
+function loadLoginForm() {
+    var loginHtml = `
+        <div class="form">
+          <h4>LOG IN</h4>
+            <p>
+              <label for="username">USERNAME</label>
+              <input name="username" id="username" title="USERNAME" maxlength="25" type="text" size="40" autofocus />
+            </p>
+            <p>
+              <label for="password">PASSWORD</label>
+              <input name="password" id="password" autocomplete="off" type="password" maxlength="50" size="40" />
+            </p>
+            <div class="btn">
+              <input id="submit_btn" type="submit" value="Login" />
+            </div>
+        </div>
+        `;
+    document.getElementById('login_area').innerHTML = loginHtml;
     
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
+    var submit = document.getElementById('submit_btn');
+    submit.onclick = function() {
+        //make a request to the server and send the name
+        //create a request object.
+        var request = new XMLHttpRequest();
+        
+        //capture the response and store it in a variable.
+        request.onreadystatechange = function() {
+            if(request.readyState === XMLHttpRequest.DONE)
+            {
+                //take some action
+                if(request.status === 200)
+                {
+                    submit.value = 'Sucess!';
+                } else if (request.status === 403) {
+                    submit.value = 'Invalid credentials. Try again?';
+                } else if (request.status === 500) {
+                    alert('Something went wrong on server side');
+                    submit.value = 'Login';
+                }
+                else {
+                    console.log(username, password);
+                    alert('Something went wrong on the server');
+                    submit.value = 'Login';
+                }
+                loadLogin();
+            }
+        };
+        //make a request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        //var name = nameInput.value;
+        console.log(username);
+        console.log(password);
+        //request.open('GET', 'http://abhishek1036cse16.imad.hasura-app.io/submit-name?name=' + name, true);
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));
+        submit.value = 'Logging in ....';
+        //capture a list of names and render it as list
+    };
 }
 
-img.onclick = function() {
-    var interval = setInterval(moveRight, 50);
-};
-*/
-// Counter code
-
-
-/*
-var button = document.getElementById('counter');
-//var counter = 0;
-
-button.onclick = function() {
-    //create a request object.
+function loadLogin() {
+    // Check if the user is already logged in
     var request = new XMLHttpRequest();
-    
-    //capture the response and store it in a variable.
-    request.onreadystatechange = function() {
-        if(request.readyState == XMLHttpRequest.DONE)
-        {
-            //take some action
-            if(request.status == 200)
-            {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                loadLoggedInUser(this.responseText);
+            } else {
+                loadLoginForm();
             }
         }
-        //if not do nothing
     };
     
-    //make a request
-    request.open('GET', 'http://abhishek1036cse16.imad.hasura-app.io/counter', true);
+    request.open('GET', '/check-login', true);
     request.send(null);
-    
-    //render the variable in the correct span.
-    //counter = counter + 1;
-    //var span = document.getElementById('count');
-    //span.innerHTML = counter.toString();
-};
-*/
+}
 
-//submit username, password
-var submit = document.getElementById('submit_btn');
-submit.onclick = function() {
-    //make a request to the server and send the name
-    //create a request object.
-    var request = new XMLHttpRequest();
-    
-    //capture the response and store it in a variable.
-    request.onreadystatechange = function() {
-        if(request.readyState == XMLHttpRequest.DONE)
-        {
-            //take some action
-            if(request.status == 200)
-            {
-                alert('logged in successfully');
-                /*
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list = '';
-                for (var i = 0; i < names.length; i++) {
-                    list += '<li>' + names[i] + '</li>'; 
-                }
-                var ul = document.getElementById('namelist');
-                ul.innerHTML = list;*/
-               // var res;
-                //res.send('<script>window.location.href="your URL";</script>');
-            } else if (request.status === 403) {
-                console.log(username, password);
-                alert('Username/password is incorrect');
-            } else if (request.status === 500) {
-                alert('Something went wrong on server side');
-        }
-        else  {
-                console.log(username, password);
-                alert('Username/password is incorrect');
-            }
-        //if not do nothing
-        }
-    };
-    //make a request
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    //var name = nameInput.value;
-    console.log(username);
-    console.log(password);
-    //request.open('GET', 'http://abhishek1036cse16.imad.hasura-app.io/submit-name?name=' + name, true);
-    request.open('POST', 'http://abhishek1036cse16.imad.hasura-app.io/login', true);
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.send(JSON.stringify({username: username, password: password}));
-    //capture a list of names and render it as list
-};
+loadLogin();
